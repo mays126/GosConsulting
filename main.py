1fa54a62a794afbf0b2d37a13e3f552354d65edd
mays126 <carbon.vom@gmail.com>
2025-05-21 10:25:59 +0300

    Api final-final commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routers.user import router as user_router
 from contextlib import asynccontextmanager
 from models.core import init_models
@@ -17,3 +19,17 @@ app.include_router(
     prefix=""
 )
 
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:5173",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
